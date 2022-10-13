26e95c5e417d89d75538a0b87335014ed4ae8302
thanhnha024 <113956295+thanhnha024@users.noreply.github.com>
2022-10-13 11:32:26 +0700

    done setup

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/assets/sass/app.scss',
+                'resources/assets/js/app.js',
             ],
             refresh: true,
         }),
